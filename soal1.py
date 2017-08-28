ed430d9b2d9007fc6275742bf03c323bd352b163
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2017-08-28 18:05:27 +0700

    Create soal1.py

---
+++ soal1.py
@@ -0,0 +1,9 @@
+# LIBRARY
+from random import randint
+
+# VARIABLE
+# Berisi angka secara random dari 0 - 100 sebanyak 100 angka
+arr = [randint(1,100) for i in range(0,100)]
+
+# FUNGSI INSERTION SORT
+# Tulis algoritma disini...
