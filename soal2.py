1bac39b0fc876cdc723a49d1b016f69b95c6a870
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2017-08-28 23:05:21 +0700

    Update soal2.py

--- soal2.py
+++ soal2.py
@@ -3,21 +3,12 @@
 inputan = input("Masukkan: ")
 
 # KELAS STACK
-class Stack:
-	def __init__(self):
-		self.items = []
-	def isEmpty(self):
-		return self.items == []
-	def push(self, item):
-		self.items.append(item)
-	def pop(self):
-		return self.items.pop()
-	def peek(self):
-		return self.items[len(self.items)-1]
-	def size(self):
-		return len(self.items)
-	def printStack(self):
-		print (self.items)
+# Buatlah kelas Stack dengan 1 atribut yaitu array, lalu memiliki fungsi sbg berikut:
+# isEmpty() -> return True jika Stack kosong dan False jika terisi.
+# push(item) -> memasukkan item kedalam Stack
+# pop() -> mengeluarkan item pada Stack
+# size() -> menghitung jumlah item pada Stack
+# printStack() -> output seluruh isi Stack
 
 # FUNGSI VALIDASI BRACKETS
-# Tulis code disini...
+# Tulis algoritma disini...
