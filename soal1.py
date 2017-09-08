426a9b77c285ac30e139e021e6682e10a7926fdd
nafasamuth <nafasa.muth@outlook.com>
2017-09-08 13:56:19 +0700

    soal1

--- soal1.py
+++ soal1.py
@@ -7,3 +7,18 @@ arr = [randint(1,100) for i in range(0,100)]
 
 # FUNGSI INSERTION SORT
 # Tulis algoritma disini...
+def insertionSort(list):
+   for index in range(1,len(list)):
+
+     currentvalue = list[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+
+list = arr
+insertionSort(arr)
+print(arr)
